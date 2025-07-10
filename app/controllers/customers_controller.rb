ab5cab4bb9a14bf8abbf5e415e26122c0e740188
ooso2 <ooso2@academic.rrc.ca>
2025-07-10 16:47:17 -0400

    Complete CRM app with customer model, ActiveAdmin, views, and custom actions

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,17 @@
+class CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def alphabetized
+    @customers = Customer.order(:full_name)
+  end
+
+  def missing_email
+    @customers = Customer.where(email_address: [nil, ""])
+  end
+end
+
+  def missing_email
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  root 'customers#index'
+
+  get "customers/index"
+  get "customers/alphabetized", to: 'customers#alphabetized', as: 'customers_alphabetized'
+  get "customers/missing_email", to: 'customers#missing_email', as: 'customers_missing_email'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
